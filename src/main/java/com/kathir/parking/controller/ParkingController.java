a1c2c55b32c889ecc0e586929fbdad29853a627f
kathirms <kathirvel.m@batonsystems>
2021-01-12 19:21:16 +0530

    www

--- src/main/java/com/kathir/parking/controller/ParkingController.java
+++ src/main/java/com/kathir/parking/controller/ParkingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:80")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ParkingController {
     @Autowired
     private ParkingService parkingService;
